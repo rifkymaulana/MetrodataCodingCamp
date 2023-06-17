ac8e89c455e57be4181f9de4e1e6d2bd9973ad0c
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-06-17 22:27:26 +0700

    feat: add EmployeeView; add HistoryView; add JobView; add LocationView

---
+++ DatabaseConnectivity/Views/EmployeeView.cs
@@ -0,0 +1,47 @@
+namespace DatabaseConnectivity.Views;
+
+class EmployeeView
+{
+    void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("++ Employee Menu ++");
+        Console.WriteLine("1. Create");
+        Console.WriteLine("2. Show All");
+        Console.WriteLine("3. Show By Id");
+        Console.WriteLine("4. Update");
+        Console.WriteLine("5. Delete");
+        Console.WriteLine("9. Back");
+        Console.Write("Please select menu: ");
+        try
+        {
+            int inputMenu = Convert.ToInt32(Console.ReadLine());
+            switch (inputMenu)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 9:
+                    break;
+                default:
+                    Console.WriteLine("Please, input 1, 2 3, 4 or 9");
+                    Console.Write("Click any key for continue...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Please, input only number not alphabet");
+            Console.Write("Click any key for continue...");
+            Console.ReadKey();
+        }
+    }
+}
