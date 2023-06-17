04640887cd28b6e5ca6f3a1702139e8507b14c9c
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-06-17 18:55:28 +0700

    feat: add authentication email and password; add feature change password

--- DatabaseConnectivity/Controllers/Authentication.cs
+++ DatabaseConnectivity/Controllers/Authentication.cs
@@ -1,25 +1,53 @@
-﻿
+﻿using DatabaseConnectivity.Models;
+
 namespace DatabaseConnectivity.Controllers;
 
 class Authentication
 {
-    /*protected static Employee employee = new Employee();
+    protected static Employee _employee = new Employee();
 
-    protected static List<Employee> employees = employee.GetAllEmployees();*/
-    public bool Login(string email, string password)
+    protected static List<Employee> employees = _employee.GetAll();
+
+
+    public bool Login(string Email, string Password)
     {
-        /*foreach (var employee in employees)
+        /*
+         * Account for login with email and password:
+         * Email = john.doe@example.com
+         * Password = 1AD
+         */
+        foreach (var employee in employees)
         {
-            if (employee.email == email && ($"{employee.id}{employee.jobId}" == password))
+            if (employee.Email == Email && ($"{employee.Id}{employee.JobId}" == Password))
             {
                 return true;
             }
-            else if (employee.email == email || ($"{employee.id}{employee.jobId}" == password))
+            else if (employee.Email == Email || ($"{employee.Id}{employee.JobId}" != Password))
             {
                 Console.WriteLine("Incorrect password");
                 return false;
             }
-        }*/
+        }
+
         return true;
+    }
+
+
+    public bool ChangePassword(string Email, string Password, string NewPassword)
+    {
+        foreach (var employee in employees)
+        {
+            if (employee.Email == Email && ($"{employee.Id}{employee.JobId}" == Password))
+            {
+                employee.Password = NewPassword;
+                return true;
+            }
+            else if (employee.Email == Email || ($"{employee.Id}{employee.JobId}" != Password))
+            {
+                Console.WriteLine("Incorrect password");
+                return false;
+            }
+        }
+        return false;
     }
 }
